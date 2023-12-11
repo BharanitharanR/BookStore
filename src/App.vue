7212e218f9921ff01536f94b0b1b37091cf4c3d7
Bharanitharan.Ragu <bharani3ran@gmail.com>
2023-12-10 19:27:50 -0500

    additional modal commits

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,66 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <h1>My first</h1>
+  <div id="appVue" @Load="submitToShowModal">
+    <h1>{{  title  }}</h1>
+    <Modal v-if="propOpenModal" :title="modalTitle" :message="modalMessage" :showButton="modalWithButton"/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import  Modal  from "./components/Modal.vue"
 
 export default {
   name: 'App',
+  data() {
+      return {
+          title: "Books List",
+          message: "Modal prompt to be shown",
+          modalTitle: "Do you have a book in mind?",
+          modalMessage: "I know you have books in shelf and not in mind",
+          modalVue: true,
+          modalWithButton: true,
+          propOpenModal: false
+      }
+  },
   components : {
-    HelloWorld
+    Modal
+  },
+  mounted() {
+    this.submitToShowModal();
+  },
+  methods: {
+    handleRefs(){
+      this.$refs.name.classList.add("app");
+      this.$refs.name.focus();
+    },
+    showModal() {
+      this.propOpenModal = !this.propOpenModal ;
+    },
+    modalActions() {
+      console.log("Modal actions"+this.Modal.closeModal());
+    },
+    submitToShowModal() {
+      console.log("Submitting to show");
+      setTimeout(
+        this.showModal ,10000);
+    }
+
+
   }
+
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app{
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  h1{
+    border-bottom-width: 0px;
+  }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="backdrop" v-if="showModal">
+        <div class="modal" >
+            <h1>{{  title }}</h1>
+            <p> {{ message }}</p>
+            <button v-if="showButton">
+                Close 
+            </button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Modal',
+    props: ["title","message","theme","showButton","buttonText"],
+    data() {
+      return {          
+          showModal: true
+      }
+    },
+    method: {
+        closeModal() {
+            console.log("close within modal" + this.showModal);
+            this.showModal = !this.showModal;
+        }
+    }
+} 
+</script>
+<style>
+.backdrop{
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0,0.5);
+    width: 100%;
+    height: 100%;
+}
+.modal{
+    width: 400px;
+    padding: 20px;
+    background-color: rgb(240, 234, 234);
+    margin: 150px auto;
+    border-radius: 10px;
+}
+.modal h1{
+    font-style: normal;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+</style>
